060bafe241dbab0a25c8395acf6572470bbc2aa8
Jonathan Cuendet <jonascuendet@gmail.com>
2019-06-27 19:11:39 +0100

    work in progress

--- src/org/academiadecodigo/bootcamp/ChatServer.java
+++ src/org/academiadecodigo/bootcamp/ChatServer.java
@@ -1,6 +1,50 @@
 package org.academiadecodigo.bootcamp;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ChatServer {
 
+    private final int portNumber;
+    private ServerSocket serverSocket;
+    private ExecutorService fixedPool;
+    private Socket clientSocket;
 
+    public ChatServer(int portNumber) {
+        this.fixedPool = Executors.newFixedThreadPool(10);
+        this.portNumber = portNumber;
+        init();
+    }
+
+    public void init() {
+        try {
+            serverSocket = new ServerSocket(portNumber);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        startListening();
+    }
+
+    private void startListening() {
+
+        try {
+
+            while (!serverSocket.isClosed()) {
+
+
+                clientSocket = serverSocket.accept();
+
+                        fixedPool.submit(new clientHandler(clientSocket));
+
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        fixedPool.shutdown();
+    }
 }
---
+++ src/org/academiadecodigo/bootcamp/Server.java
@@ -0,0 +1,8 @@
+package org.academiadecodigo.bootcamp;
+
+public class Server {
+    public static void main(String[] args) {
+        ChatServer chatServer = new ChatServer(8000);
+        chatServer.init();
+    }
+}
